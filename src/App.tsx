27e6503363e9eb0d1d13216c701f5429a3c56129
Wesley Sousa <wesleysilvadesousa@gmail.com>
2025-08-21 22:04:25 -0300

    feat: add audio toggle button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useSpaceInvaders } from './hooks/useSpaceInvaders';
+import { setSoundEnabled } from './sound';
 
 export default function App() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   useSpaceInvaders(canvasRef, { autoPlay: true });
-  return <canvas ref={canvasRef} />;
+
+  const [soundEnabled, setSoundEnabledState] = useState(true);
+
+  const toggleSound = () => {
+    const newEnabled = !soundEnabled;
+    setSoundEnabledState(newEnabled);
+    setSoundEnabled(newEnabled);
+  };
+
+  return (
+    <>
+      <canvas ref={canvasRef} />
+      <button
+        onClick={toggleSound}
+        style={{ position: 'absolute', top: 10, left: 10, zIndex: 1 }}
+      >
+        {soundEnabled ? 'Mute' : 'Unmute'}
+      </button>
+    </>
+  );
 }
